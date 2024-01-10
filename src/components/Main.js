dca591a1db419a5cd695bf14022e51a922582be3
Aquila0911 <siddharth.ghosh9@gmail.com>
2024-01-11 00:25:25 +0530

    Project 1

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Main() {
+    return <main>
+        <h1 className="main--title">Fun facts about React</h1>
+        <ul className="main--facts">
+            <li>A house cat is genetically 95.6% tiger.</li>
+            <li>Cats can jump 5 times their height</li>
+            <li>Each cat's nose is unique, much like human fingerprints.</li>
+            <li>Cats can dream</li>
+            <li>An ailurophile is a person who loves cats.</li>
+        </ul>
+    </main>
+}
+
+export default Main;
